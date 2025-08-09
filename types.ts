cd3f80e07310251027be67c1b1668c12bea831be
Róisín Grannell <r.grannell2@gmail.com>
2025-08-09 20:36:50 +0100

    builds

--- types.ts
+++ types.ts
@@ -3,19 +3,6 @@ export type Thing = string | URN;
 export type Relation = string;
 export type Triple = [Thing, Relation, Thing];
 
-export function isURN(
-  thing: Thing,
-  namespace: string = "urn:ró",
-): thing is URN {
-  return typeof thing === "string" && thing.startsWith(`urn:${namespace}:`);
-}
-
-export type Pattern =
-  // exact matches
-  | string
-  | // predicates on the value
-  ((val: string) => boolean);
-
 export type TripleObject = Record<string, string | string[]>;
 export type Predicate = (val: string) => boolean;
 
